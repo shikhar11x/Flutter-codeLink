267664a99913250864edecb8347d4a919c3610f9
Shikhar bajpai <142872564+shikhar-blaziee@users.noreply.github.com>
2026-06-30 09:54:32 +0530

    Permanent vercel.json fix

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "371601202" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3131889685" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });

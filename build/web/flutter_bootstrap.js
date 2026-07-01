8f2820d26ca12310afe7a82b282689d7c86400ae
Shikhar bajpai <142872564+shikhar-blaziee@users.noreply.github.com>
2026-07-01 11:47:52 +0530

    Uploading

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "45506955" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2927729828" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
